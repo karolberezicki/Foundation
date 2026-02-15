41b785992809a7199c26354661c4061c976d1638
Karol Berezicki <karolberezicki@gmail.com>
2026-02-15 20:16:42 +0100

    Remove commented-out campaign service and external login dead code
    ICampaignService and OWIN external login APIs were previously removed.
    This cleans up all commented-out remnants and deletes two orphaned
    ExternalLoginConfirmationViewModel files that had no remaining callers.

--- src/Foundation/Features/Api/PublicApiController.cs
+++ src/Foundation/Features/Api/PublicApiController.cs
@@ -14,7 +14,6 @@ public class PublicApiController : Controller
     private readonly IAddressBookService _addressBookService;
     private readonly LocalizationService _localizationService;
     private readonly ICustomerService _customerService;
-    //private readonly ICampaignService _campaignService;
     private readonly IUrlResolver _urlResolver;
     private readonly ICmsTrackingService _cmsTrackingService;
     private readonly IHttpContextAccessor _httpContextAccessor;
@@ -23,7 +22,6 @@ public class PublicApiController : Controller
         IContentLoader contentLoader,
         IAddressBookService addressBookService,
         ICustomerService customerService,
-        //ICampaignService campaignService,
         IUrlResolver urlResolver,
         ICmsTrackingService cmsTrackingService,
         IHttpContextAccessor httpContextAccessor)
@@ -32,7 +30,6 @@ public class PublicApiController : Controller
         _contentLoader = contentLoader;
         _customerService = customerService;
         _addressBookService = addressBookService;
-        //_campaignService = campaignService;
         _urlResolver = urlResolver;
         _cmsTrackingService = cmsTrackingService;
         _httpContextAccessor = httpContextAccessor;
@@ -142,97 +139,6 @@ public class PublicApiController : Controller
         ModelState.AddModelError("LoginViewModel.Password", _localizationService.GetString("/Login/Form/Error/WrongPasswordOrEmail", "You have entered wrong username or password"));
         return View("~/Features/Login/Index.cshtml", Url.GetUserViewModel(returnUrl));
     }
-
-    //[HttpPost]
-    //[AllowAnonymous]
-    //[ValidateAntiForgeryToken]
-    //public ActionResult ExternalLogin(string provider, string returnUrl) =>
-    //    // Request a redirect to the external login provider
-    //    new ChallengeResult(provider, Url.Action("ExternalLoginCallback", new { returnUrl }));
-
-    //[AllowAnonymous]
-    //public async Task<ActionResult> ExternalLoginCallback(string returnUrl)
-    //{
-    //    var loginInfo = await _customerService.GetExternalLoginInfoAsync();
-
-    //    if (loginInfo == null)
-    //    {
-    //        return Redirect("/");
-    //    }
-
-    //    // Sign in the user with this external login provider if the user already has a login
-    //    var result = await _customerService.SignInManager().ExternalSignInAsync(loginInfo, isPersistent: false);
-
-    //    switch (result)
-    //    {
-    //        case SignInStatus.Success:
-    //            return RedirectToLocal(returnUrl);
-
-    //        case SignInStatus.LockedOut:
-    //            return RedirectToAction("Lockout", "Login");
-
-    //        case SignInStatus.RequiresVerification:
-    //            return RedirectToAction("SendCode", "Login", new { ReturnUrl = returnUrl, RememberMe = false });
-    //        default:
-    //            // If the user does not have an account, then prompt the user to create an account
-    //            ViewBag.ReturnUrl = returnUrl;
-    //            ViewBag.LoginProvider = loginInfo.Login.LoginProvider;
-
-    //            return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { ReturnUrl = returnUrl });
-    //    }
-    //}
-
-    //[HttpPost]
-    //[AllowAnonymous]
-    //[ValidateAntiForgeryToken]
-    //public async Task<ActionResult> ExternalLoginConfirmation(ExternalLoginConfirmationViewModel viewModel)
-    //{
-    //    if (User.Identity.IsAuthenticated)
-    //    {
-    //        return RedirectToAction("Index", "Manage");
-    //    }
-
-    //    if (ModelState.IsValid)
-    //    {
-    //        // Get the information about the user from the external login provider
-    //        var socialLoginDetails = await _customerService.GetExternalLoginInfoAsync();
-    //        if (socialLoginDetails == null)
-    //        {
-    //            return View("ExternalLoginFailure");
-    //        }
-
-    //        var eMail = socialLoginDetails.ExternalIdentity.Claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Email))?.Value;
-    //        var names = socialLoginDetails.ExternalIdentity.Name.Split(' ');
-    //        var firstName = names[0];
-    //        var lastName = names.Length > 1 ? names[1] : string.Empty;
-
-    //        var user = new SiteUser
-    //        {
-    //            UserName = eMail,
-    //            Email = eMail,
-    //            FirstName = firstName,
-    //            LastName = lastName,
-    //            RegistrationSource = "Social login",
-    //            NewsLetter = viewModel.Newsletter,
-    //            IsApproved = true
-    //        };
-
-    //        var result = await _customerService.CreateUser(user);
-    //        if (result.IdentityResult.Succeeded)
-    //        {
-    //            var identityResult = await _customerService.UserManager().AddLoginAsync(user.Id, socialLoginDetails.Login);
-    //            if (identityResult.Succeeded)
-    //            {
-    //                await _customerService.SignInManager().SignInAsync(user, isPersistent: false, rememberBrowser: false);
-    //                return RedirectToLocal(viewModel.ReturnUrl);
-    //            }
-    //        }
-
-    //        AddErrors(result.IdentityResult.Errors);
-    //    }
-
-    //    return View(viewModel);
-    //}
 
     [HttpPost]
     public ActionResult TrackHeroBlock(string blockId, string blockName, string pageName)
--- src/Foundation/Features/MyAccount/OrderConfirmation/OrderConfirmationController.cs
+++ src/Foundation/Features/MyAccount/OrderConfirmation/OrderConfirmationController.cs
@@ -1,16 +1,13 @@
 ﻿using Foundation.Features.Checkout.Services;
 using Foundation.Features.MyAccount.AddressBook;
 using Foundation.Infrastructure.Commerce.Customer.Services;
-//using Foundation.Infrastructure.Services;
 
 namespace Foundation.Features.MyAccount.OrderConfirmation;
 
 public class OrderConfirmationController : OrderConfirmationControllerBase<OrderConfirmationPage>
 {
-    //private readonly ICampaignService _campaignService;
     private readonly IContextModeResolver _contextModeResolver;
     public OrderConfirmationController(
-        //ICampaignService campaignService,
         IConfirmationService confirmationService,
         IAddressBookService addressBookService,
         IOrderGroupCalculator orderGroupCalculator,
@@ -19,7 +16,6 @@ public class OrderConfirmationController : OrderConfirmationControllerBase<Order
         base(new ConfirmationService(ServiceLocator.Current.GetInstance<IOrderRepository>(), ServiceLocator.Current.GetInstance<ICurrentMarket>()), 
             addressBookService, orderGroupCalculator, urlResolver, customerService)
     {
-        //_campaignService = campaignService;
         _contextModeResolver = contextModeResolver;
     }
     public ActionResult Index(OrderConfirmationPage currentPage, string notificationMessage, int? orderNumber)
@@ -38,9 +34,6 @@ public class OrderConfirmationController : OrderConfirmationControllerBase<Order
         {
             var viewModel = CreateViewModel(currentPage, order);
             viewModel.NotificationMessage = notificationMessage;
-
-            //_campaignService.UpdateLastOrderDate();
-            //_campaignService.UpdatePoint(decimal.ToInt16(viewModel.SubTotal.Amount));
 
             return View(viewModel);
         }
